30945e039370f1d35af1b8fd20830913e697dfba
Huuhuy93 <huynguyen20101993@gmail.com>
2022-04-01 16:23:52 +0700

    ss3

---
+++ ss4_views_&_thymeleaf/exercise/manager_product_thymeleaf/src/main/java/com/service/IProductService.java
@@ -0,0 +1,22 @@
+package com.service;
+
+import com.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+
+    List<Product> findAll();
+
+    void save(Product product);
+
+    Product findById(int id);
+
+    void update(int id, Product product);
+
+    void remove(int id);
+
+    List<String> findNameProduct();
+
+    List<Product> search(String name);
+}
